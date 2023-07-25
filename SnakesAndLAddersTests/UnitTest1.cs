1d6961932114d618946c5ab0f473ba5ec1488daa
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-25 16:55:06 +0200

    red: Move Should Return 1

---
+++ SnakesAndLAddersTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+
+namespace SnakesAndLAddersTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Move_Should_Return_1()
+        {
+            var result = Move(0, 1);
+            result.Should().Be(1);
+        }
+
+        public int Move(int position, int movement)
+        {
+            return 0;
+        }
+
+    }
+}
