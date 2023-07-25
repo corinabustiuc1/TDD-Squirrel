2272cc8b274bfaa53157194936ea0090a278eb5f
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-25 17:20:00 +0200

    refactor: make Move static

--- SnakesAndLAddersTests/UnitTest1.cs
+++ SnakesAndLAddersTests/UnitTest1.cs
@@ -15,18 +15,26 @@ namespace SnakesAndLAddersTests
             Assert.Pass();
         }
 
+
+
         [TestCase(0,1,1)]
         [TestCase(0,3,3)]
+        [TestCase(4,3,7)]
+        [TestCase(14,3,17)]
         public void Move_Should_Return_ExpectedField(int position, int movement, int expected)
         {
             var result = Move(position, movement);
             result.Should().Be(expected);
         }
 
-        public int Move(int position, int movement)
+        public static int Move(int position, int movement)
         {
             return position + movement;
         }
+
+
+
+
 
     }
 }
