ff8a86c91150aa1035b62a386f61249cb99c4b2c
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-26 11:51:21 +0200

    refactor :  extract DiceRoller and Use Interface

--- SnakesAndLAddersTests/UnitTest1.cs
+++ SnakesAndLAddersTests/UnitTest1.cs
@@ -1,13 +1,18 @@
 using FluentAssertions;
 using SnakesAndLadders;
+using FakeItEasy;
 
 namespace SnakesAndLAddersTests
 {
     public class Tests
     {
+        private PieceMover _sut; // System Under Test
+        private IDiceRoller _diceRoller;
         [SetUp]
         public void Setup()
         {
+            _diceRoller = A.Fake<IDiceRoller>();
+            _sut = new PieceMover(_diceRoller);
         }
 
         [Test]
@@ -15,8 +20,6 @@ namespace SnakesAndLAddersTests
         {
             Assert.Pass();
         }
-
-
 
         [TestCase(0,1,1)]
         [TestCase(0,3,3)]
@@ -32,7 +35,8 @@ namespace SnakesAndLAddersTests
         [Test]
         public void RollDie_Should_Return_IntInRange1To6()
         {
-            var result = PieceMover.RollDie();
+            var diceRoller = new DiceRoller();
+            var result = diceRoller.RollDie();
             result.Should().BeOfType(typeof(int));
             result.Should().BeInRange(1, 6);
 
@@ -41,10 +45,11 @@ namespace SnakesAndLAddersTests
         [Test]
         public void RollDie_Should_Return_AllIntegersBetween1And6()
         {
+            var diceRoller = new DiceRoller();
             var results = new List<int>();
             for (var i = 0; i < 100; i++)
             {
-                var result = PieceMover.RollDie();
+                var result = diceRoller.RollDie();
                 results.Add(result);
             }
 
@@ -56,11 +61,13 @@ namespace SnakesAndLAddersTests
             results.Should().Contain(6);
         }
 
+
         [Test]
-        public void RollDieAndMove_Should_Return_RangeBetween1And6()
+        public void RollDieAndMove_Should_Return_4()
         {
-            var result = PieceMover.RollDieAndMove(0);
-            result.Should().BeInRange(1, 6);
+            A.CallTo(() => _diceRoller.RollDie()).Returns(4);
+            var result = _sut.RollDieAndMove(0);
+            result.Should().Be(4);
         }
     }
 }
--- SnakesAndLadders/PieceMover.cs
+++ SnakesAndLadders/PieceMover.cs
@@ -1,10 +1,40 @@
+using System.Collections;
+
 namespace SnakesAndLadders;
+
+public class DiceRoller : IDiceRoller
+{
+    private Random _random;
+
+    public DiceRoller()
+    {
+        _random = new Random() ;
+    }
+
+    public int RollDie()
+    {
+        var result = _random.Next(1,7);
+        return result;
+    }
+}
+
+public interface IDiceRoller
+{
+    int RollDie();
+}
 
 public class PieceMover
 {
-    public static int RollDieAndMove(int position)
-    { 
-        var random = RollDie();
+    private IDiceRoller _diceRoller;
+
+    public PieceMover(IDiceRoller diceRoller)
+    {
+        _diceRoller = diceRoller;
+    }
+
+    public int RollDieAndMove(int position)
+    {
+        var random = _diceRoller.RollDie();
         var newPosition = Move(position, random);
 
         return newPosition;
@@ -13,12 +43,5 @@ public class PieceMover
     public static int Move(int position, int movement)
     {
         return position + movement;
-    }
-
-    public static int RollDie()
-    {
-        var random  = new Random();
-        var result = random.Next(1,7);
-        return result;
     }
 }
